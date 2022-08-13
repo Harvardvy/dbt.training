13a79cf9cc061b4dcbb73db0200e7261677bc541
harvard.thisis@gmail.com <harvard.thisis@gmail.com>
2022-08-13 07:22:38 +0000

    join of orders and customers

--- models/staging/stage_orders.sql
+++ models/staging/stage_orders.sql
@@ -1,2 +1,7 @@
-select *,(ordersellingprice -ordercostprice) as profit
+select 
+--raw_orders
+orderid,orderdate,shipdate,shipmethod,(ordersellingprice -ordercostprice) as profit,
+--raw_customers
+customername,segment,country,state
  from {{ ref('raw_orders') }}
+ left join {{ ref('raw_customer') }}
