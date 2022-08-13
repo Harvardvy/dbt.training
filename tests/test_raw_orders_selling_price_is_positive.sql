73c4256f48bca36bfb54e31936d5bf3d0248941b
harvard.thisis@gmail.com <harvard.thisis@gmail.com>
2022-08-13 22:53:07 +0000

    test to check positive values of  order selling price

--- tests/test_raw_orders_selling_price_is_positive.sql
+++ tests/test_raw_orders_selling_price_is_positive.sql
@@ -1,8 +1,7 @@
 with 
-orders as {
-select * from {{ ref('raw_orders') }}
-
-}
+orders as (
+    select * from {{ ref('raw_orders') }}
+)
 
 select orderid,sum(ordersellingprice) as order_sp from
 orders group by orderid having order_sp<0 
