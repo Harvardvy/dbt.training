3443ac6d849d121ec1d6a381943f7e5bb7f721c7
harvard.thisis@gmail.com <harvard.thisis@gmail.com>
2022-08-13 07:55:27 +0000

    col alignment

--- models/reporting/reporting_profit_by_product.sql
+++ models/reporting/reporting_profit_by_product.sql
@@ -1,4 +1,4 @@
-select productid,productname,category,subcategory,sum(profit)
+select productid,productname,category,subcategory,sum(profit) as amount
 from {{ ref('stage_orders') }}
 group by 
 productid,productname,category,subcategory
